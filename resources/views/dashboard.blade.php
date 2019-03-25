24a7ecc54acdb9f371b54448d73d008413f2746e
Erwin Rahayu <artmxare@gmail.com>
2019-03-25 18:30:14 +0700

    Add Menu

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('style')
+
+@endsection()
+
+@section('sidebar')
+    @parent
+
+    @include('layouts.sidebar')
+@endsection
+
+@section('content')
+@endsection
+
+@section('script')
+@endsection
